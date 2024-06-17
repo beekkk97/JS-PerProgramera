0d49541be3dda7479acbff7a68fb6ff2ceace5cb
Bekim Memeti <114069904+beekkk97@users.noreply.github.com>
2024-06-18 00:52:43 +0200

    Add files via upload

---
+++ tdee calculator/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [     
+    "./**/*.{html, js}",     
+    //"./index.html", 
+    //"./**/*.{html, js, ts}", 
+    //"./src/**/*.{html, js, jsx, ts, tsx}",   
+],   
+  theme: {
+    fontFamily: {
+      'Montserrat': ['montserrat',],
+    }
+  },
+  plugins: [],
+}
